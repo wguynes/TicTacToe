c9ef6423be584157788c1aeeec0f0d7ddedd4694
William Guynes <wguynes@hy-vee.com>
2022-01-20 12:27:25 -0600

    Refactor production

--- python/main.py
+++ python/main.py
@@ -1,36 +1,45 @@
+from enum import Enum
+
 INCOMPLETE_GAME = -1
 CATS_GAME = 0
 DIMENSION_OF_BOARD = 3
 
 
-def tic_tac_toe(board):
+class Direction(Enum):
+    DESCENDING = 1
+    ASCENDING = 2
 
-    paths = [
+
+def tic_tac_toe(board):
+    possible_win_paths = [
         *board,
-        *[[board[j][i] for j in range(DIMENSION_OF_BOARD)] for i in range(3)],
-        [board[i][i] for i in range(DIMENSION_OF_BOARD)],
-        [board[DIMENSION_OF_BOARD-1-i][i] for i in range(DIMENSION_OF_BOARD)]
+        *transpose(board),
+        diagonal_to_list(board, Direction.DESCENDING),
+        diagonal_to_list(board, Direction.ASCENDING)
     ]
 
-    for row in paths:
-        winner = find_any_winner(row)
+    for path in possible_win_paths:
+        winner = find_any_winner(path)
         if winner is not None:
             return winner
 
-    list_of_all_board_spaces = [x for row in board for x in row]
-    if contains_a_zero(list_of_all_board_spaces):
-        return INCOMPLETE_GAME
+    all_board_spaces = [x for row in board for x in row]
+    return INCOMPLETE_GAME if 0 in all_board_spaces else CATS_GAME
 
-    return CATS_GAME
+
+def diagonal_to_list(board, direction):
+    return [board[i if direction == Direction.DESCENDING else DIMENSION_OF_BOARD - 1 - i][i]
+            for i in range(DIMENSION_OF_BOARD)]
+
+
+def transpose(board):
+    return [[board[j][i] for j in range(DIMENSION_OF_BOARD)] for i in range(DIMENSION_OF_BOARD)]
 
 
 def find_any_winner(values):
-    if not contains_a_zero(values):
-        sum_of_values = sum(values)
-        if sum_of_values % DIMENSION_OF_BOARD == 0:
-            return sum_of_values / DIMENSION_OF_BOARD
+    if 0 not in values:
+        value_set = set(values)
+        if len(value_set) == 1:
+            return next(iter(value_set), None)
+
     return None
-
-
-def contains_a_zero(values):
-    return any([x == 0 for x in values])
